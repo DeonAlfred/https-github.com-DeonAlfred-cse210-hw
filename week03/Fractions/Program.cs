4956cb463a60827df9440f3e77695e117e340b14
Deon alfred <deonjohannes1984@gmail.com>
2026-09-15 18:50:26 -0700

    commit fract

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction = new Fraction(3, 4);
+
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
+    }
+    
+}
+
+public class Fraction
+{
+    // Attributes
+    private int _top;
+    private int _bottom;
+
+    // Default constructor
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    // Constructor with one parameter
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    // Constructor with two parameters
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Getters and setters
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    // Return the fraction as a string
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    // Return the fraction as a decimal
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
     }
 }
